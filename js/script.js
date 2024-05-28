58fc0878f6dfb2af94d4b5f1672808431ec65e7b
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-05-28 17:05:20 -0400

    update for twitter-clone

--- js/script.js
+++ js/script.js
@@ -24,5 +24,20 @@ document.addEventListener("DOMContentLoaded", function () {
     // Attach navigation functions to arrow buttons
     document.querySelector(".carousel-navigation.prev").onclick = prevSlide;
     document.querySelector(".carousel-navigation.next").onclick = nextSlide;
+
+    const iframe = document.getElementById('embedded-website');
+
+    window.addEventListener('message', (event) => {
+        // Verify the origin of the message
+        if (event.origin !== 'https://twitterclone.sacenpapier.synology.me/') return;
+
+        // Handle the message
+        console.log('Message from iframe:', event.data);
+    });
+
+    // Send a message to the iframe
+    iframe.onload = () => {
+        iframe.contentWindow.postMessage('Hello from parent page', 'https://twitterclone.sacenpapier.synology.me/');
+    };
   });
   
